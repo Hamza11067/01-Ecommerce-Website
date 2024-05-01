7a2ca11aab649a8ce44330c973c9532be8dea8f7
Hamza11067 <hamzakhalid1067@gmail.com>
2024-05-01 15:48:22 +0500

    responsive home page completed

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Script for navigation bar
+// toggle navigation bar
+let tl = gsap.timeline();
+let menu = document.querySelector("#menu-icon");
+let close = document.querySelector("#menubar i");
+
+tl.to("#menubar", {
+  right: 0,
+  duration: 0.5,
+});
+
+tl.from("#menubar a", {
+  x: 100,
+  duration: 0.6,
+  stagger: 0.12,
+  opacity: 0,
+});
+
+tl.from("#menubar i", {
+  opacity: 0,
+});
+
+tl.pause();
+
+menu.addEventListener("click", function () {
+  tl.play();
+});
+
+close.addEventListener("click", function () {
+  tl.reverse();
+});
